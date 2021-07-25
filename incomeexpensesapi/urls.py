ed528b344349fb909fba07874d8db854d25a601b
Abdou K. Sene <a_sene@salemstate.edu>
2021-07-25 13:14:29 -0400

    2.3

--- incomeexpensesapi/urls.py
+++ incomeexpensesapi/urls.py
@@ -20,26 +20,27 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Income Expense API",
-      default_version='v1',
-      description="An API for income expenses",
-      terms_of_service="https://www.yourco.com/terms/",
-      contact=openapi.Contact(email="contact@income.remote"),
-      license=openapi.License(name="Test License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+# SWAGGER UI
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="Income Expense API",
+#       default_version='v1',
+#       description="An API for income expenses",
+#       terms_of_service="https://www.yourco.com/terms/",
+#       contact=openapi.Contact(email="contact@income.remote"),
+#       license=openapi.License(name="Test License"),
+#    ),
+#    public=True,
+#    permission_classes=(permissions.AllowAny,),
+# )
 
 # The first thing to get called
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Sends them to urls.py in authentication
     path('auth/', include('authentication.urls')),
-    path('', schema_view.with_ui('swagger', 
-                                    cache_timeout=0), name='schema-swagger-ui'),
-    path("redoc", schema_view.with_ui('redoc', 
-                                        cache_timeout=0), name='schema-redoc'),
+    # path('', schema_view.with_ui('swagger', 
+    #                                 cache_timeout=0), name='schema-swagger-ui'),
+    # path("redoc", schema_view.with_ui('redoc', 
+    #                                     cache_timeout=0), name='schema-redoc'),
 ]
